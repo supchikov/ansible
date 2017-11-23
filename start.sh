b94fac313446b626bc070a695da55f3c5311c0e6
root <root@otrs.stand.icluster.pro>
2017-11-23 16:42:55 +0300

    Edit start.sh script 4

--- start.sh
+++ start.sh
@@ -2,4 +2,6 @@
 
 mkdir /home/git
 cd /home/git
-yum install git ansible -y
+yum install git ansible openssh -y
+git clone https://github.com/supchikov/ansible.git
+yes | cp -rf /home/git/ansible/ansible.cfg /etc/ansible/ansible.cfg
