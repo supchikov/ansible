f9f4d5cbdd4bdc86aa7d75dee0d33437bef3d58e
root <root@otrs.stand.icluster.pro>
2017-11-23 17:10:18 +0300

    Add some files

--- start.sh
+++ start.sh
@@ -7,3 +7,7 @@ git clone https://github.com/supchikov/ansible.git
 yes | cp -rf /home/git/ansible/ansible.cfg /etc/ansible/ansible.cfg
 ssh-keygen -t rsa
 ssh-copy-id root@localhost
+rm -rf /etc/ansible/roles 
+ln -s /home/git/ansible/roles /etc/ansible/ 
+rm -rf /etc/ansible/playbooks
+ln -s /home/git/ansible/playbooks /etc/ansible/ 
